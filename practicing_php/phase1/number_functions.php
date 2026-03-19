d7b0ccbf3ecdc688e3b6a708d2ca374fc66efbb9
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-19 11:34:53 +0100

    checking the type of a number by using built in functions

---
+++ practicing_php/phase1/number_functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1 = 10;
+echo(is_int($num1));
+
+echo "</br>";
+$num2 = 12.5;
+
+echo(is_float($num2));
+
+$num3 = 1222222223333333333333;
+
+echo "</br>";
+echo(is_finite($num3));
+?>
