17e2144da22bc52643727b1329e1cc704f308466
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-19 11:26:28 +0100

    data types in php

---
+++ practicing_php/phase1/data_type.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = 10;
+$num2 = 10.5;
+$name = "temesgen";
+$istrue = TRUE;
+$programming_languages = array("java","c++","javascript","python","C","php");
+
+var_dump($num1);
+var_dump($num2);
+var_dump($programming_languages);
+
+?>
