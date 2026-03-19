5614563bbb7da6f1ba93cba2a7f96b01bb9bbe52
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-19 17:30:56 +0100

    some simple math lib functions in php

---
+++ practicing_php/phase1/math_function.php
@@ -0,0 +1,15 @@
+<?php
+
+echo pi();
+echo "</br>";
+$numbers = array(3,10,15,28,300,1000);
+echo min($numbers);
+echo "</br>";
+echo max($numbers);
+echo "</br>";
+echo sqrt(625);
+echo "</br>";
+echo round(3.4);
+echo "</br>";
+echo rand()
+?>
