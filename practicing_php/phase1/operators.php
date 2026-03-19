ab1e74370105aa98d3750f261307277e836ddf28
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-19 17:31:41 +0100

    operators in php

---
+++ practicing_php/phase1/operators.php
@@ -0,0 +1,61 @@
+<?php
+$num1 = 25;
+$num2 = 10;
+$num3 = 3;
+$num4 = 4;
+
+// artihmetic operators  
+
+echo $num1 + $num2;
+echo "</br>";
+echo $num1 - $num2;
+echo "</br>";
+echo $num1 * $num2;
+echo "</br>";
+echo $num1 / $num2;
+echo "</br>";
+echo $num1 % $num2;
+echo "</br>";
+echo $num3 ** $num4;
+echo "</br>";
+
+// assignment operators  x = x + y means x += y
+
+echo "</br>";
+echo $num1 += $num2;
+echo "</br>";
+echo $num1 -= $num2;
+echo "</br>";
+echo $num1 *= $num2;
+echo "</br>";
+echo $num1 /= $num2;
+echo "</br>";
+echo $num1 %= $num2;
+echo "</br>";
+echo $num3 ** $num4;
+
+// comparision operators 
+
+echo "</br>";
+echo $num1 == $num2;
+echo "</br>";
+echo $num1 != $num2;
+echo "</br>";
+echo $num1 > $num2;
+echo "</br>";
+echo $num1 < $num2;
+echo "</br>";
+echo $num1 >= $num2;
+echo "</br>";
+echo $num3 <= $num4;
+
+// string operator
+
+echo "</br>";
+
+$txt1 = "hello";
+$txt2 = " ";
+$txt3 = "world";
+
+echo $txt1 . $txt2 . $txt3;
+?>
