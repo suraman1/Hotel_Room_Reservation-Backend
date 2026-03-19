1f5d5db828e70522063d80595c4b8b1a52ce2aa4
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-19 11:33:47 +0100

    simple string functions

---
+++ practicing_php/phase1/String_functions.php
@@ -0,0 +1,18 @@
+
+<?php
+$name = "temesgen";
+$mname = "belay girma";
+$lname = "girma";
+
+
+echo(strlen($name));
+echo "</br>";
+echo(str_word_count($mname));
+echo "</br>";
+echo(strrev($name));
+echo "</br>";
+echo(strpos($mname,"gir"));
+echo "</br>";
+echo(str_replace("girma","gizaw",$mname));
+
+?>
