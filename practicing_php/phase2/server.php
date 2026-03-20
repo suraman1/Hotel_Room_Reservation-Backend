d48d22a34ddefcc4d680fc51a4a482d147df0d4f
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-20 21:59:39 +0100

    another global variable(SERVER)

---
+++ practicing_php/phase2/server.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<h1> server side info</h1>";
+echo "<br>";
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo $_SERVER['SCRIPT_FILENAME'];
+echo "<br>";
+echo $_SERVER['DOCUMENT_ROOT'];
+echo "<br>";
+?>
